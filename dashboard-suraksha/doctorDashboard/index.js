b8d36f24ed2e4633e733511763ea074be2aec88c
Suraksha More <125144399+SurakshaMore@users.noreply.github.com>
2023-08-17 07:20:29 +0530

    Add files via upload

---
+++ dashboard-suraksha/doctorDashboard/index.js
@@ -0,0 +1,6 @@
+const docImg = document.getElementById("docImg1");
+const docName = document.getElementById("docname1");
+const docEdu = document.getElementById("docEdu1");
+const docAge = document.getElementById("docAge1");
+const docFee = document.getElementById("editIcon1");
+const editIcon = document.getElementById("editIcon1");
